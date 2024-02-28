1cd7d405f62fcd429b6566d6b4fee81c4f1ce858
Chapderlane <154477996+Chapderlane@users.noreply.github.com>
2024-02-28 06:29:58 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♦", "♥", "♠", "♣"];
+  const randomSuitIndex = Math.floor(Math.random() * suits.length);
+  const randomSuit = suits[randomSuitIndex];
+
+  const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const randomNumberIndex = Math.floor(Math.random() * numbers.length);
+  const randomNumber = numbers[randomNumberIndex];
+
+  const selectNumberDiv = document.getElementById("number");
+  const selectIcon1Div = document.getElementById("icon1");
+  const selectIcon2Div = document.getElementById("icon2");
+  selectNumberDiv.innerHTML = randomNumber;
+  selectIcon1Div.innerHTML = randomSuit;
+  selectIcon2Div.innerHTML = randomSuit;
 };
